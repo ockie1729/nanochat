830d18b03e5597d4ae2e1527ec123a8be345fb01
OKIMOTO Yusuke <you_oki1@yahoo.co.jp>
2026-09-22 11:39:14 +0900

    実験スクリプトのGPU数を変更

--- runs/speedrun_2_train_base.sh
+++ runs/speedrun_2_train_base.sh
@@ -29,7 +29,7 @@ DEPTH=3
 WANDB_RUN="d${DEPTH}"
 
 # d24 model (slightly undertrained to beat GPT-2 => decrease data:params ratio from compute optimal 10.5 (default) to 8)
-torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=$DEPTH --target-param-data-ratio=8 --device-batch-size=16 --fp8 --run=$WANDB_RUN
+torchrun --standalone --nproc_per_node=1 -m scripts.base_train -- --depth=$DEPTH --target-param-data-ratio=8 --device-batch-size=16 --fp8 --run=$WANDB_RUN
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
-torchrun --standalone --nproc_per_node=8 -m scripts.base_eval -- --device-batch-size=16
+torchrun --standalone --nproc_per_node=1 -m scripts.base_eval -- --device-batch-size=16
 
